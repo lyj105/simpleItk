0c6cefad16908da3f3771296c49d731e6c737b16
lyj105 <1093683539@qq.com>
2019-07-10 14:59:08 +0800

    初始itk学习day

---
+++ testItkGithub/testItkGithub.cpp
@@ -0,0 +1,17 @@
+﻿// testItkGithub.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "itkImage.h" 
+#include "itkImageFileReader.h" 
+#include "itkImageFileWriter.h"
+
+#include "ImportLib.h"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
